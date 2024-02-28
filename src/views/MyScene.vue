ce4b01c9f8031a121232578e4d18541e066eaa89
Jacen <jacenwang0610@gmail.com>
2024-02-28 11:36:14 +0800

    commit

--- src/views/MyScene.vue
+++ src/views/MyScene.vue
@@ -7,73 +7,99 @@
 
 <script>
 import * as THREE from 'three'
+import ROSLIB from 'roslib'
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {PCDLoader} from "three/examples/jsm/loaders/PCDLoader";
+import {toRaw} from "vue";
 
 export default {
   name: 'MyScene',
+  data() {
+    return {
+      scene: null,
+      camera: null,
+      renderer: null,
+      cube: null,
+      controls: null,
+
+
+      rosClient: null
+    };
+  },
   mounted() {
     this.initScene();
+    this.loadPCD();
+    this.initRos();
+    this.startAnimation();
   },
   methods: {
     initScene() {
-      // 创建场景
-      const scene = new THREE.Scene()
-      scene.background = new THREE.Color(0x404040) // 使用RGB表示灰色，这里的值为0x404040
+      this.scene = new THREE.Scene();
+      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      this.camera.position.z = 5;
 
-      // 创建相机
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-      camera.position.z = 50;
-
-
-
-      // 创建渲染器
-      const renderer = new THREE.WebGLRenderer()
-      renderer.setSize(window.innerWidth, window.innerHeight)
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
 
       // 将渲染器添加到DOM中
-      this.$refs.scene.appendChild(renderer.domElement)
+      this.$refs.scene.appendChild(this.renderer.domElement);
 
-      // 添加坐标轴辅助线
-      const axesHelper = new THREE.AxesHelper(5)
-      scene.add(axesHelper)
-
-      //相机控制器
-      const controls = new OrbitControls(camera,renderer.domElement);
-
-      //车辆模型，先用长方体表示
-      const boxgeo = new THREE.BoxGeometry( 1, 1, 1 );
-      const boxmat = new THREE.MeshBasicMaterial( {color: 0xff0000} );
-      const carModel = new THREE.Mesh( boxgeo, boxmat );
-      carModel.position.set(3,0,0);
-      scene.add(carModel);
-
-      const pcdLoader = new PCDLoader();
-      pcdLoader.load('20230710all.pcd',
-      function(points) {
-            scene.add(points);
-            },function (xhr) {
-            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-            },
-          function () {
-            console.log('Error Occurred');
-      });
-
-
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    }
+    ,
+    startAnimation() {
       // 动画循环
       const animate = () => {
-        requestAnimationFrame(animate)
+        requestAnimationFrame(animate);
 
+        // 更新 OrbitControls
+        this.controls.update();
 
-        /*carModel.position.x += 0.01;*/
-
-        //更新控制
-        controls.update();
         // 渲染场景
-        renderer.render(scene, camera)
+        this.renderer.render(toRaw(this.scene), this.camera);
       }
 
-      animate()
+      animate();
+    }
+    ,
+    loadPCD(){
+      const pcdLoader = new PCDLoader();
+      const scene = this.scene;
+      pcdLoader.load('20230710all.pcd',
+          function(points) {
+            scene.add(points);
+          },function (xhr) {
+            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+          },
+          function (e) {
+            console.log('Error Occurred',e);
+          }
+      );
+    }
+    ,
+    initRos() {
+      this.rosClient = new ROSLIB.Ros(
+          {url: 'ws://localhost:9090'}
+      );
+      this.rosClient.on('connection', function() {
+        console.log('Connected to websocket server.');
+      });
+
+      this.rosClient.on('error', function(error) {
+        console.log('Error connecting to websocket server: ', error);
+      });
+
+      this.rosClient.on('close', function() {
+        console.log('Connection to websocket server closed.');
+      });
+      let pointTopic = new ROSLIB.Topic({
+        ros: this.rosClient,
+        name: '/points_raw',
+        messageType: 'sensor_msgs/PointCloud2'
+      });
+      pointTopic.subscribe(function (msg) {
+        console.log(1);
+      })
     }
   }
 }
