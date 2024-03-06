ef4055bc97ae583c3019d770d2f62df27c591f83
Jacen <jacenwang0610@gmail.com>
2024-03-06 10:12:57 +0800

    MENU add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,56 @@
 <template>
-  <div class="real-center-container">
-    <div class="large-text" >Jacen Holdings</div>
-    <h1 style="color: #42b983">Welcome to Jacen's Vue test Project</h1>
-    <h2 style="color: cadetblue">Most power by Three.js</h2>
-    <div>
-      <router-link class="btn-container" to="/myScene">
-        <el-button size="large" type="warning" round>Go to MyScene</el-button>
-      </router-link>
-      <router-link class="btn-container" to="/partScene">
-        <el-button size="large" type="success" round>Go to TestScene</el-button>
-      </router-link>
-      <router-link class="btn-container" to="/mqttTest">
-        <el-button size="large" type="primary" round>Go to MqttTest</el-button>
-      </router-link>
-    </div>
+  <el-container>
+    <el-header height="80px">
+      <JMenu></JMenu>
+    </el-header>
 
-  </div>
+    <el-container>
+      <el-main>
+        <div class="large-text" >Jacen Holdings</div>
+        <h1 style="color: #42b983">Welcome to Jacen's Vue test Project</h1>
+        <h2 style="color: cadetblue;">Power by </h2>
+        <el-icon size="80px" style="margin: 0 auto"><ChromeFilled /><ElementPlus /></el-icon>
+
+      </el-main>
+    </el-container>
+
+  </el-container>
 </template>
 
 <script>
+import JMenu from "@/views/JMenu.vue";
+import {ChromeFilled, ElementPlus} from "@element-plus/icons-vue";
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {ChromeFilled, ElementPlus, JMenu},
+  data() {
+    return {
+      isCollapse: false
+    }
+  }
 }
 </script>
 
 <style scoped>
 
 
-  .real-center-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 80vh;
-  }
+.real-center-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+}
 
-  .large-text {
-    font-size: 80px;
-    color: aquamarine;
-  }
+.large-text {
+  font-size: 80px;
+  color: aquamarine;
+}
 
-  .btn-container {
-    flex-direction: row;
-    margin: 20px;
+.btn-container {
+  flex-direction: row;
+  margin: 20px;
 
-  }
+}
 </style>
---
+++ src/views/JMenu.vue
@@ -0,0 +1,69 @@
+<template>
+<el-menu
+    default-active="0"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    :router="true"
+>
+<el-menu-item index="0">
+  LOGO
+</el-menu-item>
+<div class="flex-grow" />
+<el-menu-item index="1">Processing Center</el-menu-item>
+<el-sub-menu index="2">
+  <template #title>Workspace</template>
+  <el-menu-item index="/myScene">My Scene</el-menu-item>
+  <el-menu-item index="/partScene">Part Scene</el-menu-item>
+  <el-menu-item index="/mqttTest">MQTT Test</el-menu-item>
+  <el-sub-menu index="2-4">
+    <template #title>item four</template>
+    <el-menu-item index="2-4-1">item one</el-menu-item>
+    <el-menu-item index="2-4-2">item two</el-menu-item>
+    <el-menu-item index="2-4-3">item three</el-menu-item>
+  </el-sub-menu>
+</el-sub-menu>
+</el-menu>
+</template>
+
+
+<script>
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting, ChatRound, House, Tools, ElementPlus,
+} from '@element-plus/icons-vue'
+
+export default {
+  name: 'JMenu',
+  data() {
+    return {
+
+    }
+  },
+  props: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 250px;
+  min-height: 600px;
+}
+
+
+ .flex-grow {
+   flex-grow: 1;
+ }
+
+
+
+</style>
